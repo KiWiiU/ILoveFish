670a55f7419dbef15e0644bf0881ba9a6bffef05
Francesca Bruno (KiWiiU) <135619100+KiWiiU@users.noreply.github.com>
2025-04-26 11:52:42 -0400

    stuff

--- Assets/Scripts/MotivationalSpeech/TextSpeechSmiley.cs
+++ Assets/Scripts/MotivationalSpeech/TextSpeechSmiley.cs
@@ -1,34 +1,35 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class TextSpeechSmiley : MonoBehaviour
 {
-    public TMPro_Text textBox;
-    private static string[] motivationalTexts = {
-        "Don't let your fish be fish.",
+    public TMP_Text textBox;
+    public string[] motivationalTexts = {
         "Just keep swimming.",
-        "Even the smallest fish can make the biggest waves.";
+        "Even the smallest fish can make the biggest waves.",
         "You weren't born to stay in the fish bowl: explore the ocean.",
         "A smooth sea never made a skilled fish.",
         "Seas the day.",
         "You're fintastic the way you are.",
         "The world is your oyster.",
         "Don't be afraid to swim against the school.",
-        "A fish doesn't wait for the tide, it swims where it needs to go.",
+        "A fish doesn't wait for the tide, it swims where it needs to go."
     };
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(RefreshMotivationalQuote);
+        StartCoroutine("RefreshMotivationalQuote");
     }
 
     public IEnumerator RefreshMotivationalQuote() {
-        Random rnd = new Random();
+        System.Random rnd = new System.Random();
         while(true) {
-            textBox.text = motivationalTexts[rnd.Next(0, motivationalTexts.Length)];
-            yield return new WaitForSeconds(3f);
+            textBox.text = motivationalTexts[(int)rnd.Next(0, motivationalTexts.Length)];
+            yield return new WaitForSeconds(8.5f);
         }
     }
 }
